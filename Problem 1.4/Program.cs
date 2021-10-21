cf0bf42813c7665463e732bbdb0921edced1615e
GamuchiraiS <80484981+GamuchiraiS@users.noreply.github.com>
2021-10-21 16:41:40 +0200

    Add files via upload

---
+++ Problem 1.4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter;
+
+            Console.WriteLine("Please enter a letter: ");
+            letter = Convert.ToChar(Console.ReadLine().ToLower());
+
+            if (letter == 'a' || letter == 'e' ||
+                letter == 'i' || letter == 'o' ||
+                letter == 'u') 
+            {
+                Console.WriteLine(letter + " is a vowel");
+            }
+
+            else
+            {
+                Console.WriteLine(letter + " is a consonant");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
